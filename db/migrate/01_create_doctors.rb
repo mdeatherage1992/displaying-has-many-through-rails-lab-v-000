b00cf95e7f819ddf82e11faa759f8de2af011fcb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-10 14:06:02 +0000

    automatically backed up by learn

--- db/migrate/01_create_doctors.rb
+++ db/migrate/01_create_doctors.rb
@@ -3,8 +3,8 @@ class CreateDoctors < ActiveRecord::Migration
     create_table :doctors do |t|
       t.string :name
       t.string :department
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
       t.timestamps null: false
     end
   end
