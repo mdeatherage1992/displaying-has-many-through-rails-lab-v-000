9403455c17c46877d4fd23b58b0aa2039c2bcf3f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-10 14:06:32 +0000

    automatically backed up by learn

--- db/migrate/02_create_appointments.rb
+++ db/migrate/02_create_appointments.rb
@@ -1,10 +1,11 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.string :name
-      t.string :department
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
+      t.integer :patient_id
+      t.integer  :doctor_id
+      t.datetime :appointment_datetime
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
       t.timestamps null: false
     end
   end
